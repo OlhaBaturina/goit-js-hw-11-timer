db535815981783516fad98bd5e6acbf796961a30
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-05-26 00:44:10 +0300

    Подключает счетчик к интерфейсу

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,6 @@ const hoursRef = document.querySelector('[data-value="hours"]');
 const minsRef = document.querySelector('[data-value="mins"]');
 const secsRef = document.querySelector('[data-value="secs"]');
 
-console.log(daysRef);
-
-// Плагин это класс CountdownTimer,
-// экземпляр которого создает новый таймер с настройками.
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
-});
-
 const timer = {
   start() {
     const startTime = Date.now();
@@ -21,8 +12,10 @@ const timer = {
     setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = startTime - currentTime;
-      const timeComponents = getTimeComponents(deltaTime);
-      console.log(timeComponents);
+      const time = getTimeComponents(deltaTime);
+
+      updateClockface(time);
+      console.log(`${days}:${hours}:${mins}:${secs}`);
       // console.log('00:00:00');
     }, 1000);
   },
@@ -32,16 +25,12 @@ console.log(timer);
 timer.start();
 
 function updateClockface({ days, hours, mins, secs }) {
-  refs.clockface.textContent = `${days}:${hours}:${mins}:${secs}`;
-
-  // daysRef = days;
-  // hoursRef = hours;
-  // minsRef = mins;
-  // secsRef = secs;
+  daysRef.textContent = `${days}`;
+  hoursRef.textContent = `${hours}`;
+  minsRef.textContent = `${mins}`;
+  secsRef.textContent = `${secs}`;
 }
-console.log(updateClockface);
-// Для подсчета значений используй следующие готовые формулы,
-// где time - разница между targetDate и текущей датой.
+
 function pad(value) {
   return String(value).padStart(2, '0');
 }
@@ -55,23 +44,7 @@ function getTimeComponents(time) {
   return { days, hours, mins, secs };
 }
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jan 01, 2022'),
+});
